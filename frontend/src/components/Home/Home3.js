463817d1c15c01a679d4960ed98ddb905b626562
rociogzm22 <125653371+rociogzm22@users.noreply.github.com>
2024-10-17 19:42:22 +0200

    Home y arreglos

--- frontend/src/components/Home/Home3.js
+++ frontend/src/components/Home/Home3.js
@@ -3,9 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import i1 from "../../Assets/Home/embalse1.jpeg";
 import i2 from  "../../Assets/Home/ai2.jpeg";
 import i3 from  "../../Assets/Home/estadisticas.jpeg";
-import Type from "./Type";
 import { Link } from "react-router-dom";
-
 function Home3() {
   return (
     <section>
@@ -16,22 +14,17 @@ function Home3() {
                     ¿Qué puedes hacer con <span className="main-name"> Polish Cow</span>?
                 </h1>
                 <Row>
-                    {/* Primera Columna */}
-                    <Col md={4}>
-                    <img
-                        src={i1}
-                        alt="Imagen 1"
-                        className="img-fluid"
-                    />
-                    <h5 className="mt-3">Encuentra embalses cercanos</h5>
-                    <p>Localiza fácilmente todos los embalses dentro de un radio ajustable de hasta 100 km,
-                        usando tus coordenadas GPS o ingresando una ubicación manualmente.
-                    </p>
-                    <Link to="/embalses">
-                        <span style={{ cursor: "pointer", textDecoration: "underline"}}>
-                        Descubre más
-                        </span>
-                    </Link>
+        {/* Primera Columna */}
+        <Col md={4}>
+          <img
+            src={i1}
+            alt="Imagen 1"
+            className="img-fluid"
+          />
+          <h4 className="titulo">Encuentra embalses cercanos</h4>
+          <p>Localiza fácilmente todos los embalses dentro de un radio ajustable de hasta 100 km,
+            usando tus coordenadas GPS o ingresando una ubicación manualmente.
+          </p>
                     </Col>
 
                     {/* Segunda Columna */}
@@ -41,13 +34,10 @@ function Home3() {
                         alt="Imagen 2"
                         className="img-fluid"
                     />
-                    <h5 className="mt-3">Predice la escasez de agua</h5>
+                    <h5 className="titulo">Predice la escasez de agua</h5>
                     <p>Anticipa posibles problemas de escasez de agua evaluando la cantidad embalsada en los embalses cercanos,
                        y ajusta tus planes en función de la predicción.
                     </p>
-                    <a href="https://enlace-2.com" target="_blank" rel="noopener noreferrer">
-                        Descubre más
-                    </a>
                     </Col>
 
                     {/* Tercera Columna */}
@@ -57,13 +47,10 @@ function Home3() {
                         alt="Imagen 3"
                         className="img-fluid"
                     />
-                    <h5 className="mt-3">Consulta estadísticas detalladas</h5>
+                    <h5 className="titulo">Consulta estadísticas detalladas</h5>
                     <p>Accede a estadísticas clave como valores anuales máximos, mínimos y medias para cada embalse, demarcación o área,
                        y realiza filtrados por diferentes atributos.
                     </p>
-                    <a href="https://enlace-3.com" target="_blank" rel="noopener noreferrer">
-                        Descubre más
-                    </a>
                     </Col>
                 </Row>
             </Container>
